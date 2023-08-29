f6b02a3423e6b5f845eae8fbe1efec1e1ad3c52d
Sagar Dhaduk <sagardhaduk1@gmail.com>
2023-08-29 12:59:06 -0500

    "Deploy on Vercel"

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ let playerName = "";
 const pool = mysql.createPool({
     host: process.env.HOST,
     user: process.env.USER, 
+    password: process.env.PASSWORD,
     database: process.env.DATABASE,
+    waitForConnections: true,
 }).promise()
 
 async function getData() {
