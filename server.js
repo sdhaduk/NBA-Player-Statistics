f279fba45024229da69579972dff2ea3dfd20f8c
Sagar Dhaduk <sagardhaduk1@gmail.com>
2023-08-29 12:05:00 -0500

    "Switched from SQLite3 to MySQL"

--- server.js
+++ server.js
@@ -11,25 +11,31 @@ app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
+
 let playerName = "";
 
 const pool = mysql.createPool({
     host: process.env.HOST,
     user: process.env.USER, 
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
-}).promise(); 
+    database: process.env.DATABASE,
+}).promise()
 
+async function getData() {
 
-async function getStats() {
+    console.log("Getting Query")
+
     const [rows] = await pool.query(`
     SELECT player, tm, pos, g, mp_per_game, fg_per_game, fga_per_game, fg_percent, x3p_percent,
-    x2p_percent, ft_percent, orb_per_game, drb_per_game, trb_per_game, ast_per_game, blk_per_game, tov_per_game, pts_per_game 
+    x2p_percent, ft_percent, orb_per_game, drb_per_game, trb_per_game, ast_per_game, blk_per_game, tov_per_game, pts_per_game, stl_per_game
     FROM player_data
     WHERE player = ?`, [playerName]);
 
-    return rows
+    return rows[0]
 };
+
+
+
+
 
 app.get("/", async (req, res) => {
 
@@ -41,11 +47,17 @@ app.get("/", async (req, res) => {
         PTSPG: "---"});
 
     } else {
-        const stats = await getStats();
-        console.log(stats)
+
+        const stats = await getData();
+
+        res.render("body", {Name: stats.player, Team: stats.tm, Pos: stats.pos, 
+        GP: stats.g, MIN: stats.mp_per_game, FGPG: stats.fg_per_game, FGAPG: stats.fga_per_game, FGP: stats.fg_percent, 
+        THREEPP: stats.x3p_percent, TWOPP: stats.x2p_percent, FTP: stats.ft_percent, ORBPG: stats.orb_per_game, DRBPG: stats.drb_per_game, 
+        TRBPG: stats.trb_per_game, APG: stats.ast_per_game, STLPG: stats.stl_per_game, BLKPG: stats.blk_per_game, TOVPG: stats.tov_per_game, 
+        PTSPG: stats.pts_per_game});
     }
-        
 });
+
 
 app.post("/", (req, res) => {
 
@@ -53,6 +65,7 @@ app.post("/", (req, res) => {
     res.redirect("/")
 
 });
+
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, function() {
